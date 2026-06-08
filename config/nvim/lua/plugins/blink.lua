a7ed3d31018cc49300867121f5998ab3bfcc915e
Benoît Legat <benoit.legat@gmail.com>
2026-06-08 14:22:45 +0200

    Add comment

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,11 @@
+-- The default in `omarchy` is the `enter` preset.
+-- For accepting auto-complete suggestions, this preset require <enter>. Two issues with this
+-- It is not consistent with VS-code which uses TAB
+-- When at the end of a line, I press <enter> to get a newline and it
+-- then accepts the auto-complete, this is annoying
 return {
-  "saghen/blink.cmp",
-  opts = {
-    keymap = { preset = "super-tab" },
-  },
+	"saghen/blink.cmp",
+	opts = {
+		keymap = { preset = "super-tab" },
+	},
 }
