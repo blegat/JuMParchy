74d910dd3d0b9688181f67dca7a7c580e27b018d
Benoît Legat <benoit.legat@gmail.com>
2026-06-17 15:53:49 +0200

    Faster check for groups

--- bin/latex.sh
+++ bin/latex.sh
@@ -7,7 +7,7 @@ ROOT_SRC="$(cd "$SCRIPT_DIR/.." && pwd)"
 # See [here](https://github.com/basecamp/omarchy/discussions/1720)
 omarchy-pkg-add zathura zathura-pdf-mupdf
 # texlive is a pkg group so we cannot use omarchy-pkg-add
-#"$SCRIPT_DIR/pkg-add" texlive
+"$SCRIPT_DIR/pkg-add" texlive
 link ".latexmkrc" "$ROOT_SRC/.latexmkrc" "$HOME"
 # config.sh also copies
 # - config/nvim/lua/plugins/latex.lua
