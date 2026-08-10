0ad15b4e871bdc47f6389c8e06b2ce5d61a36475
Benoît Legat <benoit.legat@gmail.com>
2026-08-10 08:55:20 +0200

    Add Julia install

--- bin/julia.sh
+++ bin/julia.sh
@@ -1,3 +1,15 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/lib.sh"
+
+# Until https://github.com/basecamp/omarchy/pull/5890 is merged
+if command -v julia > /dev/null; then
+    ok "julia"
+else
+    doing "julia"
+    curl -fsSL https://install.julialang.org | sh -s -- --yes
+fi
+
 # config.sh also copies
 # - config/nvim/lua/plugins/flash.lua
 # - config/nvim/lua/plugins/julia-repl.lua
