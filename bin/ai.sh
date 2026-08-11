d3b7a02341283ab57c16d98f7de0b3df7e9d9331
Benoît Legat <benoit.legat@gmail.com>
2026-08-11 22:59:00 +0200

    Fix

--- bin/ai.sh
+++ bin/ai.sh
@@ -27,6 +27,26 @@ else
   printf '[tool]\nname = "opencode"\npermission_mode = "bypass"\n' >"$coi_opencode_profile"
 fi
 
+# See https://github.com/mensfeld/code-on-incus/issues/83#issuecomment-4065301139
+ensure_iptables_rule() {
+  local description="$1"
+  local chain="$2"
+  shift 2
+
+  if sudo iptables -C "$chain" "$@"; then
+    ok "$description"
+  else
+    doing "$description"
+    sudo iptables -I "$chain" "$@"
+  fi
+}
+
+ensure_iptables_rule "Allow Incus DHCP" INPUT -i incusbr0 -p udp --dport 67 -j ACCEPT
+ensure_iptables_rule "Allow Incus DNS (UDP)" INPUT -i incusbr0 -p udp --dport 53 -j ACCEPT
+ensure_iptables_rule "Allow Incus DNS (TCP)" INPUT -i incusbr0 -p tcp --dport 53 -j ACCEPT
+ensure_iptables_rule "Allow Incus bridge inbound forwarding" FORWARD -i incusbr0 -j ACCEPT
+ensure_iptables_rule "Allow Incus bridge outbound forwarding" FORWARD -o incusbr0 -j ACCEPT
+
 doing "Build COI image"
 coi build
 
