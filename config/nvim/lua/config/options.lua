e2d036dd60d22d6b775ca001d8ad86701b6c8fdf
Benoît Legat <benoit.legat@gmail.com>
2026-08-15 15:47:49 +0200

    Revert "Load omarchy options"
    This reverts commit cce8b553f453de6f322392fc74e6854ab98a4714.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,11 +1,5 @@
 -- [REPLACES_COPY_OF] /usr/share/omarchy-nvim/config/lua/config/options.lua
 
--- The stock options.lua requires sibling modules from the package tree, but
--- omarchy-nvim-setup only seeds ~/.config/nvim when it doesn't exist yet, so
--- modules added by later package updates never land there. Make the package's
--- lua dir a fallback for require; ~/.config/nvim still wins via runtimepath.
-package.path = "/usr/share/omarchy-nvim/config/lua/?.lua;" .. package.path
-
 dofile("/usr/share/omarchy-nvim/config/lua/config/options.lua")
 
 vim.opt.tabstop = 4
